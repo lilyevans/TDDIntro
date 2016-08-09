e762ce4857bf18ab28b675cc68fe766d875a8822
lilyevans <evanslm2@g.cofc.edu>
2016-08-09 07:27:55 -0500

    tests 1, 2,3, 4 pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -4,6 +4,13 @@ public class Factorial {
     public Integer compute(int i) {
         if (i == 0)
             return 1;
-        return i;
+        else {
+            int fact = 1;
+            for (int n = 1; n <= i; n++){
+               fact *= n;
+           }
+            return fact;
+        }
+
     }
 }
